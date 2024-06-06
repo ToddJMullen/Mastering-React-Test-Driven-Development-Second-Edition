2f4e177b97f2097f33bed069bccdcccaecbc380d
Todd Mullen <ToddJ.Mullen@gmail.com>
2024-06-06 06:55:24 +0300

    Chapter 2 pt 1 & 2

--- Chapter02/Start/src/Appointment.js
+++ Chapter02/Start/src/Appointment.js
@@ -3,3 +3,31 @@ import React from "react";
 export const Appointment = ({ customer }) => (
   <div>{customer.firstName}</div>
 );
+
+export const AppointmentsDayView = ({
+  appointments,
+}) => (
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map((appt) => (
+        <li key={appt.startsAt}>
+          {getTime(appt.startsAt)}
+        </li>
+      ))}
+    </ol>
+    {appointments.length === 0 ? (
+      <p>
+        There are no appointments scheduled for today.
+      </p>
+    ) : (
+      <Appointment {...appointments[0]} />
+    )}
+  </div>
+);
+
+const getTime = (startsAt) => {
+  const [h, m] = new Date(startsAt)
+    .toTimeString()
+    .split(":");
+  return `${h}:${m}`;
+};
